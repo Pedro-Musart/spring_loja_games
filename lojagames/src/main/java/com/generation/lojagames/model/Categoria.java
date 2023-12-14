c1908a471846f700d9fe459b86c8b0fc1dfbf9a8
Pedro Soares <pedrowesleyfs@gmail.com>
2023-12-14 14:48:12 -0300

    criado o CRUD da classe Categoria

---
+++ lojagames/src/main/java/com/generation/lojagames/model/Categoria.java
@@ -0,0 +1,57 @@
+package com.generation.lojagames.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 1, max = 60, message = "O atributo nome deve conter no mínimo 05 e no máximo 60 caracteres")
+	private String nome;
+	
+//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+//	@JsonIgnoreProperties("categoria")
+//	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+	
+	
+	
+	
+	
+}
